6090b2d2aefb059a2cc74866e47ab78726565bfa
Kurstie DeHaven-Vargas <kurstiedehaven@gmail.com>
2024-03-06 17:00:42 -0500

    added reaction & index models

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// import user and though models from their respective files
+const User = require('./User');
+const Thought = require('./Thought');
+
+// export the models
+module.exports = { User, Thought };
---
+++ models/reaction.js
@@ -0,0 +1,33 @@
+const { Shema, model, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+// export the Reaction model
+module.exports = model('Reaction', reactionSchema);
