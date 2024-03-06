5e08a6846dbeeb6ccd2ba508e43e4c2923d60818
Kurstie DeHaven-Vargas <kurstiedehaven@gmail.com>
2024-03-06 17:11:04 -0500

    added user model

---
+++ models/thought.js
@@ -0,0 +1,39 @@
+const { Schema, model, Types } = require('mongoose');
+const reactionSchema = require('./reactionSchema');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => new Date(timestamp).toDateString() // format date
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+// export the Thought model
+module.exports = Thought;
---
+++ models/user.js
@@ -0,0 +1,45 @@
+const { Shema, model, Types } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+// get total count of friends on retrieval
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+// create the user model using the userSchema
+const User = model('User', userSchema);
+// export the User model
+module.exports = User;
